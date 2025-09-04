2e91c45acfb7eeb788bed2d93d9141160d55aaa8
ErmakovEv <105241345+ErmakovEv@users.noreply.github.com>
2025-09-04 23:49:14 +0300

    fix: импорт

--- app/shared/ui/Comments/ui/CommentList.tsx
+++ app/shared/ui/Comments/ui/CommentList.tsx
@@ -1,9 +1,9 @@
-import { fetchComments } from '@/app/shared/lib/data';
 import { TFullComment } from '@/app/shared/types';
 import { CommentItem } from './CommentItem';
+import { fetchWorkComments } from '@/app/shared/lib/data';
 
 export const CommentList = async ({ workId }: { workId: string }) => {
-  const comments: TFullComment[] = await fetchComments(workId);
+  const comments: TFullComment[] = await fetchWorkComments(workId);
 
   return (
     <div className="flex flex-col gap-3">
